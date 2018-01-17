fedcca10d58900c0a491a54a9eaf94569f7ac41f
PHP Developer <d@winklix.com>
2018-01-17 13:56:44 +0530

    category api

--- app/config.php
+++ app/config.php
@@ -4,8 +4,10 @@
 <?php
 
 $SERVER='localhost';
+//$USERNAME='healthca_heal';
 $USERNAME='healthca_heal';
-$PASSWORD='3y.i&6MaD)y@';
+//$PASSWORD='3y.i&6MaD)y@';
+$PASSWORD='';
 $DB='healthca_health';
 
 $con=mysqli_connect($SERVER,$USERNAME,$PASSWORD,$DB) or die ("could not connect to mysql");;
